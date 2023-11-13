da176f29a0b6473cc65e10f7fa070ffd43a9ac97
jpickler <sirzymok@gmail.com>
2023-11-13 15:42:29 -0500

    added select a featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,33 @@ import './App.css'
 import {puppyList} from './data.js'
 
 function App() {
+  const [featPupId, setFeatPupId] = useState(null)
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(`this is puppies`, puppies)
+  //console.log(`this is puppies`, puppies)
+const featuredPup = puppies.find((pup) => pup.id === featPupId)
+console.log(featuredPup)
+
 
   return (
-    <>
       <div className="App">
         {
             puppies.map((puppy) => {
-              return <p key={puppy.id}>{puppy.name}</p>
-            })
-        }
+              return (
+              <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+              );
+            })} 
+            {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
       </div>
       
-    </>
   )
 }
 
